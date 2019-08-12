23498d73c20f36b609b3b4b3e33026f23ebdc937
qqchens <172524789@qq.com>
2019-08-12 18:53:32 +0800

    first commit

---
+++ src/csDataStruct/Array.java
@@ -0,0 +1,16 @@
+package csDataStruct;
+
+public class Array<E> {
+	
+	private E[] data;
+	private int size;
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
