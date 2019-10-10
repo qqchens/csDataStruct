211b4fd9f5c9cb169842db5b473095b88939c6b1
qqchens <172524789@qq.com>
2019-10-10 20:52:46 +0800

    segementTree add query and set function

--- src/csDataStruct/SegementTree.java
+++ src/csDataStruct/SegementTree.java
@@ -8,6 +8,7 @@ public class SegementTree<E> {
 	private E[] data;
 	private Merge<E> merge;
 	
+	@SuppressWarnings("unchecked")
 	public SegementTree(E[] arr, Merge<E> merge){
 		
 		this.merge = merge;
@@ -75,6 +76,67 @@ public class SegementTree<E> {
 		return res.toString();
 	}
 	
+	//统计线段树中的l-r范围
+	public E query(int l, int r){
+		
+		if(l<0 || l>=data.length || r<0 || r>=data.length || r<l){
+			throw new IllegalArgumentException("Index is illegal.");
+		}
+		
+		return query(0, 0, data.length-1, l, r);
+	}
+	
+	//递归调用，统计
+	private E query(int index, int leftIndex, int rightIndex, int l, int r){
+		if(l == r){
+			return data[l];
+		}
+		
+		int min = leftIndex + (rightIndex - leftIndex) / 2;
+		int leftChildIndex = leftChild(index);
+		int rightChildIndex = rightChild(index);
+		if(min >= r){
+			return query(leftChildIndex, leftIndex, min, l, r);
+		}else if(min + 1 <= l){
+			return query(rightChildIndex, min+1, rightIndex, l, r);
+		}
+		
+		E leftChild = query(leftChildIndex, leftIndex, min, l, min);
+		E rightChild = query(rightChildIndex, min+1, rightIndex, min+1, r);
+		return merge.merge(leftChild, rightChild);
+		
+	}
+	
+	//更新线段树的某个值
+	public void set(int index, E e){
+		if(index<0 || index>=data.length){
+			throw new IllegalArgumentException("Index is illegal.");
+		}
+		
+		data[index] = e;
+		set(0, 0, data.length-1, index, e);
+	}
+	
+	//递归调用更新
+	private void set(int root, int l, int r, int index, E e){
+		
+		if(l == r){
+			tree[root] = e;
+			return;
+		}
+		
+		int min = l + (r - l) / 2;
+		int leftChild = leftChild(root);
+		int rightChild = rightChild(root);
+		if(min >= index){
+			set(leftChild, l, min, index, e);
+		}else{
+			set(rightChild, min+1, r, index, e);
+		}
+		
+		tree[root] = merge.merge(tree[leftChild], tree[rightChild]);
+		
+	}
 
 	public static void main(String[] args) {
 		Integer[] num = {-2, 3, 5, 1, 0, 7};
@@ -86,9 +148,11 @@ public class SegementTree<E> {
 		});
 		
 		System.out.println(s);
+		s.set(3, 2);
+		System.out.println(s);
 		
 		SegementTree<Integer> s2 = new SegementTree<>(num, (a, b) -> a * b);
-		System.out.println(s2);
+		System.out.println(s2.query(0, 2));
 	}
 
 }
